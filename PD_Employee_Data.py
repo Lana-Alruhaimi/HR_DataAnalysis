e816e116de5100511c42b6e48f275b1dc3af6f50
Lana <lanaalruhaimi@gmail.com>
2025-09-17 11:48:21 +0300

    uploading files from desktop to GitHub

---
+++ PD_Employee_Data.py
@@ -0,0 +1,45 @@
+import pandas as pd
+
+df = pd.read_csv('Employee_Data.csv') #Employee data is saved as df
+print(df.info()) # results show no missing data (its already clean)
+print("\n\n\n\n**********************************************************\n")
+
+### Given Questions ###
+
+# How many total employees are there? 
+Emp_Num = df.shape[0] #number of rows (for columns replace 0 w/ 1)
+print (f"\nNumber of Employees: {Emp_Num}")
+
+# What is the employee count for each department? 
+Dept_Num = df.groupby('Department').size() #group by splits into smaller dfs, size counts rows
+print (f"\nEmployees per Department:\n{Dept_Num}")
+
+# What is the average monthly income by job role? 
+Avg_Sal_Role= df.groupby('JobRole')['MonthlyIncome'].mean().sort_values(ascending=False) #[specify column to calculate], mean is avg, sort values orders it
+print (f"\nAverage Monthly Salary per Role (Highest to lowest):\n{Avg_Sal_Role}")
+
+# Who are the top 5 employees by performance rating? 
+Top5_Rate = df.nlargest(5,'PerformanceRating')# would add ['Name'] if I only wanted to print their names, but df doesnt have names
+print (f"\nTop 5 Employees by Performance Rating:\n{Top5_Rate}")
+
+# Which department has the highest average performance rating? 
+Avg_Rate = df.groupby("Department")['PerformanceRating'].mean() #Avg per dept
+Max_Rate_Dept = Avg_Rate.idxmax() #idxmax returns the index(name) of highest dept
+Max_Rate_Num = Avg_Rate.max()
+print(f"\nThe {Max_Rate_Dept} Department has the Highest Average Performance Rating: {Max_Rate_Num}")
+
+
+
+#### My Questions ###
+
+#Avg monthly income per total working years (sorted by highest income to lowest)
+Avg_Sal_Years = df.groupby('TotalWorkingYears')['MonthlyIncome'].mean().sort_values(ascending=False)
+print(f"\nAverage Salary per Years Working (Highest to lowest):\n{Avg_Sal_Years}")
+
+#Avg salary hike per Education field (sorted by highest income to lowest)
+Avg_SalH_Edu = df.groupby('EducationField')['PercentSalaryHike'].mean().sort_values(ascending=False) #average salary hike
+print(f"\nAverage Salary Hike Percent per Education Field (Highest to lowest):\n{Avg_SalH_Edu}")
+
+#Avg monthly salary per gender
+Avg_Sal_G = df.groupby('Gender')['MonthlyIncome'].mean()
+print (f"\nAverage Salary for each Gender:\n{Avg_Sal_G}")
---
+++ Streamlit_HR.py
@@ -0,0 +1,79 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv('Employee_Data.csv')
+
+Depts = df['Department'].unique() #list of dept names (no repeating)
+Brand_Colors = ['#4f008c','#c2a6cf','#e4d9eb','#e4e9ee'] # Brand colors hex code
+
+########### Data Visualization ###########
+
+### Bar chart (Employees per Department) 
+Dept_Num = df['Department'].value_counts().reset_index() #counts emp per dept and converts to dataframe so i can use plotly express (more customization)
+Dept_Num.columns = ['Department','Number of Employees'] #label of columns i want in the bar chart
+
+Bar = px.bar( #Bar creation
+    Dept_Num,
+    x = 'Number of Employees',
+    y = 'Department',
+    color='Department', #makes each bar diff color
+    color_discrete_sequence= Brand_Colors,
+    title = "Employees per Department"
+) 
+st.plotly_chart (Bar) #to display the bar chart
+
+### Pie chart (job satisfaction)
+Sat_Num = df['JobSatisfaction'].value_counts().reset_index() #Sat_Num = number of employees per satisfaction level
+Sat_Num.columns = ['Job Satisfaction','Number of Employees']
+
+Pie = px.pie(
+    Sat_Num,
+    values='Number of Employees',
+    names = 'Job Satisfaction',
+    color= 'Job Satisfaction',
+    color_discrete_sequence=Brand_Colors,
+    title = 'Job Satisfaction Distribution',
+)
+st.plotly_chart(Pie)
+
+### Box chart (salary per education field)
+Box = px.box(
+    df,
+    x = 'EducationField', # x because it is fixed (categorical)
+    y = 'MonthlyIncome',
+    color = 'EducationField',
+    color_discrete_sequence=Brand_Colors,
+    title = 'Salary Distribution by Education Field'
+)
+st.plotly_chart(Box)
+
+### Dropdown Menu (departments)
+Selected_Dept = st.selectbox('Choose a department', Depts) #creates dropdown menu & saves users choice
+Filtered_Dept = df[df['Department']==Selected_Dept] #filters depts based on selected dept
+st.dataframe(Filtered_Dept) # Displays result as table
+
+
+########### Update Employee Data ###########
+
+### Add new employee
+st.header ("Add new employee")
+
+with st.form("New_Emp_Info"):
+    st.write("Enter Employee's Information: ")
+
+    EmployeeNumber = st.number_input("Employee Number")
+    Department = st.selectbox("Department")
+    EducationField = st.text_input("Education Field")
+    JobRole = st.text_input("Job Role")
+    MonthlyIncome = st.number_input("Monthly Income", min_value=0) #so salary isnt negative
+
+    #submitted =
+
+
+
+
+
+
+
+### Update employee salary
