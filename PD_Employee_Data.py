9d1cc6c60f11381bab44f95896cd3d1b00db075e
Lana <lanaalruhaimi@gmail.com>
2025-09-18 13:24:47 +0300

    Added clarifing comments, removed space from table name and added print table (as a comment, just to test)

--- PD_Employee_Data.py
+++ PD_Employee_Data.py
@@ -1,11 +1,15 @@
 import pandas as pd
 
+
+########### Load & Clean Data ###########
+
 df = pd.read_csv('Employee_Data.csv') #Employee data is saved as df
 df = df.dropna() #drop null values (clean data)
 print(df.info()) #print proving there are no null values
-
-#print(df_clean.info())
 print("\n\n\n\n**********************************************************\n")
+
+
+########### Data Analysis ###########
 
 ### Given Questions ###
 
@@ -18,7 +22,7 @@ print (f"\nNumber of Employees: {Emp_Num}")
 Dept_Num = df.groupby('Department').size() #group by splits into smaller dfs, size counts rows
 print (f"\nEmployees per Department:\n{Dept_Num}")
 
-# What is the average monthly income by job role? 
+# What is the average monthly income by job role? (sorted by highest income to lowest)
 Avg_Sal_Role= df.groupby('JobRole')['MonthlyIncome'].mean().sort_values(ascending=False) #[specify column to calculate], mean is avg, sort values orders it
 print (f"\nAverage Monthly Salary per Role (Highest to lowest):\n{Avg_Sal_Role}")
 
@@ -36,14 +40,14 @@ print(f"\nThe {Max_Rate_Dept} Department has the Highest Average Performance Rat
 
 #### My Questions ###
 
-#Avg monthly income per total working years (sorted by highest income to lowest)
+# What is the average monthly income per total working years? (sorted by highest income to lowest)
 Avg_Sal_Years = df.groupby('TotalWorkingYears')['MonthlyIncome'].mean().sort_values(ascending=False)
 print(f"\nAverage Salary per Years Working (Highest to lowest):\n{Avg_Sal_Years}")
 
-#Avg salary hike per Education field (sorted by highest income to lowest)
+# What is the average salary hike % per Education field (sorted by highest salary hike % to lowest)
 Avg_SalH_Edu = df.groupby('EducationField')['PercentSalaryHike'].mean().sort_values(ascending=False) #average salary hike
 print(f"\nAverage Salary Hike Percent per Education Field (Highest to lowest):\n{Avg_SalH_Edu}")
 
-#Avg monthly salary per gender
+# What is the average monthly salary per gender?
 Avg_Sal_G = df.groupby('Gender')['MonthlyIncome'].mean()
 print (f"\nAverage Salary for each Gender:\n{Avg_Sal_G}")
--- SQL_Employee_Data.py
+++ SQL_Employee_Data.py
@@ -12,14 +12,41 @@ Emp_db ='Employee_Data.db'
 try:
     conn = sql.connect(Emp_db) #create connection
     print(f"Successful connection to {Emp_db}.")
-    df.to_sql('Table of Employees', conn, if_exists='replace',index=False) #replaces current to make sure the data is as up-to-date as possible
+    df.to_sql('Table_of_Employees', conn, if_exists='replace',index=False) #replaces current to make sure the data is as up-to-date as possible
     print("Data inserted successfully.")
+
+    ### Print table
+    '''
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM Table_of_Employees")
+    rows = cursor.fetchall()
+    print("\n--------Table of Employees--------")
+    headers = [description[0] for description in cursor.description] #points cursor at column names
+    print (headers)
+
+    for row in rows:
+        print (row)
+    '''
+    ########### Data Analysis Begins ###########
+    
+    ### Given Questions ###
+
+    # How many total employees are there?
+
+    # What is the employee count for each department?
+    # What is the average monthly income by job role?
+    # Who are the top 5 employees by performance rating?
+    # Which department has the highest average performance rating?
+    #### My Questions ###
+    # What is the average monthly income per total working years? (sorted by highest income to lowest)
+    # What is the average salary hike % per Education field (sorted by highest salary hike % to lowest)
+    # What is the average monthly salary per gender?
+    
+    ########### Data Analysis Ends ###########
+
 except sql.Error as e: #saves error in var to print later (helps understand the reason for error)
     print(f"Error: {e}")
 finally:
     if 'conn' in locals() and conn:
         conn.close() # closing force saves any updates and unlocks db
         print("Database connection closed.")
-
-
-########### Data Analysis ###########
